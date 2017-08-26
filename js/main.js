f5176032c1a57291d33d7905ad6caa19f0e8c3b1
Brodie Balser <balserbrodie@gmail.com>
2017-08-26 13:53:43 -0600

    now I have the player and the bady in game

--- js/main.js
+++ js/main.js
@@ -2,19 +2,24 @@
 let gameState = {
 
   preload: function () {
-    this.load.image('background', 'assets/background.jpg')
-    this.load.image('player', 'assets/people/wizc.gif')
+    this.load.image('background', 'assets/background.png')
+    this.load.image('player', 'assets/people/wiz_0.1.png')
+    this.load.image('bady', 'assets/people/bady_0.1.png')
   },
   create: function () {
     this.background = this.game.add.sprite(0, 0, 'background')
-    this.player = this.game.add.sprite(0, 0, 'player')
+    this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player')
+    this.player.anchor.setTo(0.5, 0.5)
+
+    this.bady = this.game.add.sprite(100, 100, 'bady')
+    this.bady.anchor.setTo(0.5, 0.5)
   },
   update: function () {
 
   }
 }
 
-const game = new Phaser.Game(800, 800, Phaser.AUTO)
+const game = new Phaser.Game(500, 500, Phaser.AUTO)
 
 game.state.add('gameState', gameState)
 game.state.start('gameState')
