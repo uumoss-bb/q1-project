53e2b27fbc3c2e24bafcbed4f511a54174077ca0
Brodie Balser <balserbrodie@gmail.com>
2017-08-28 10:44:08 -0600

    the player moves

--- js/main.js
+++ js/main.js
@@ -12,19 +12,44 @@ let gameState = {
 
     this.background = this.game.add.sprite(0, 0, 'background')
     //background color for checking the size
-    this.stage.backgroundColor = "#4488AA"
-    game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+    // this.stage.backgroundColor = "#4488AA"
+
     //this is you
     this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player')
     this.player.anchor.setTo(0.5, 0.5)
+    this.player.angle = 0
+
     //this is the bady
     this.bady = this.game.add.sprite(250, 100, 'bady')
     this.bady.anchor.setTo(0.5, 0.5)
     this.bady.scale.setTo(1, -1) //this flips on Y axis
+
+
   },
 
   update: function () {
 
+    //player movement
+    if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+      this.player.scale.setTo(1,1)
+      this.player.angle = -90
+      this.player.x -= 4
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+      this.player.scale.setTo(1,1)
+      this.player.angle = 90
+      this.player.x += 4
+    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+      this.player.angle = 0
+      this.player.scale.setTo(1,1)
+      this.player.y -= 4
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+      this.player.angle = 0
+      this.player.scale.setTo(1,-1)
+      this.player.y += 4
+    }
   }
 }
 
