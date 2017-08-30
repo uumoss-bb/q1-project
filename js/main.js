0720388cd8e4b2ff64f3a51a8437ae4f6122491e
Brodie Balser <balserbrodie@gmail.com>
2017-08-29 20:53:01 -0600

    i got a new way to shoot now

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-
+var player
 var total = 0
 var mob = []
-var weapon
+var bullet;
+var bullets;
+var bulletTime = 0;
 
 let gameState = {
 
@@ -21,24 +23,34 @@ let gameState = {
     game.add.tileSprite(0, 0, 1500, 1500, 'background')
 
     //this is you
-    this.player = game.add.sprite(300, 300, 'player')
-    game.physics.arcade.enable(this.player)
-    this.player.body.collideWorldBounds = true
-    this.player.anchor.setTo(0.5, 0.5)
-    game.camera.follow(this.player)
+    player = game.add.sprite(300, 300, 'player')
+    game.physics.arcade.enable(player)
+    player.body.collideWorldBounds = true
+    player.anchor.setTo(0.5, 0.5)
+    game.camera.follow(player)
 
     badyCreation();
     this.bady = game.add.sprite(game.world.randomX, game.world.randomY, 'bady')
     this.bady.anchor.setTo(0.5, 0.5)
     game.physics.arcade.enable(this.bady)
 
-    //fireBall
-    weapon = game.add.weapon(30, 'fireBall')
-    // this kills the bullet when it leaves the bounds
-    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS
-    weapon.bulletSpeed = 600;
-    weapon.fireRate = 100;
-    weapon.trackSprite(this.player, 0, 0, true)
+    // //fireBall 1.0
+    // weapon = game.add.weapon(30, 'fireBall')
+    // // this kills the bullet when it leaves the bounds
+    // weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS
+    // weapon.bulletSpeed = 600;
+    // weapon.fireRate = 150
+    // weapon.trackSprite(player, 0, 0, true)
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+    //  All 40 of them
+    bullets.createMultiple(40, 'fireBall');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 0.5);
+
 
 
   },
@@ -46,55 +58,81 @@ let gameState = {
   update: function () {
 
     //player movement
-    this.player.rotation = game.physics.arcade.angleToPointer(this.player)
+    player.rotation = game.physics.arcade.angleToPointer(player)
     //bady movement
-    if(this.player.x && this.player.y !== this.bady.x && this.bady.y){
-       this.bady.rotation = game.physics.arcade.angleBetween(this.bady, this.player) + 1.57079633
-       game.physics.arcade.moveToObject(this.bady, this.player, 100)
+    if(player.x && player.y !== this.bady.x && this.bady.y){
+       this.bady.rotation = game.physics.arcade.angleBetween(this.bady, player) + 1.57079633
+       game.physics.arcade.moveToObject(this.bady, player, 100)
     }
     //Mob movement
     for (let a = 0; a < mob.length; a++) {
       mob[a].anchor.setTo(0.5, 0.5)
       game.physics.arcade.enable(mob[a])
-      mob[a].rotation = game.physics.arcade.angleBetween(mob[a], this.player) + 1.57079633
-       game.physics.arcade.moveToObject(mob[a], this.player, 100)
-
+      mob[a].rotation = game.physics.arcade.angleBetween(mob[a], player) + 1.57079633
+      game.physics.arcade.moveToObject(mob[a], player, 100)
     }
 
     //player controls
     if(game.input.keyboard.isDown(Phaser.Keyboard.A)){
-      this.player.x -= 4
+      player.x -= 4
     }
     else if(game.input.keyboard.isDown(Phaser.Keyboard.D)){
-      this.player.x += 4
+      player.x += 4
     }
     if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
-      this.player.y -= 4
+      player.y -= 4
     }
     else if(game.input.keyboard.isDown(Phaser.Keyboard.S)){
-      this.player.y += 4
+      player.y += 4
     }
 
-    // //bady spawner
-    // if(total < 200 && game.time.now > timer){
-    //   badyCreation();
-    // }
+    //bady spawner
+    if(total < 200 && game.time.now > timer){
+      badyCreation();
+
+    }
 
     // shooting fireBall
-    if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-      weapon.fire()
+    if(game.input.activePointer.isDown){
+      fireBullet()
     }
-    weapon.rotation = game.physics.arcade.angleToPointer(weapon)
+    //fireBall hits bady?
+    if(bullet.x && bullet.y === this.bady.x && this.bady.y){
+      badyDeath()
+    }
 
   }
 }
 
+
 function badyCreation(){
   this.bady = game.add.sprite(game.world.randomX, game.world.randomY, 'bady')
-  mob.push(this.bady)
+  mob.unshift(this.bady)
 
   total++;
   timer = game.time.now + 100;
+
+}
+function badyDeath (){
+
+  console.log('hit')
+}
+
+function fireBullet () {
+
+    if (game.time.now > bulletTime)
+    {
+        bullet = bullets.getFirstExists(false);
+
+        if (bullet)
+        {
+            bullet.reset(player.x + 16, player.y + 16);
+            bullet.lifespan = 2000;
+            bullet.rotation = player.rotation;
+            game.physics.arcade.velocityFromRotation(player.rotation, 400, bullet.body.velocity);
+            bulletTime = game.time.now + 50;
+        }
+    }
 
 }
 
