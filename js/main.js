39a3040994ecd7db7f38d480406c6d5daf6bee44
Brodie Balser <balserbrodie@gmail.com>
2017-08-30 20:55:32 -0600

    the text works correctly

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 var player
 var playerLife = 3
-var bady
+var livesString
+var lifetext
+var invincible = false
+
+var baddie
 var el
 var mob
-var bullet
-var bullets
+
 var total = 0
 var timer
+
+var bullet
+var bullets
 var bulletTime = 0
-var invincible = false
+
+var kills = 0
+var killString
+var scoreText
 
 let gameState = {
 
@@ -16,8 +25,9 @@ let gameState = {
     //my assets
     this.load.image('background', 'assets/wizBizBackG.png')
     this.load.image('player', 'assets/people/wiz_0.1.png')
-    this.load.image('bady', 'assets/people/bady_0.1.png')
+    this.load.image('baddie', 'assets/people/bady_0.1.png')
     this.load.image('fireBall', 'assets/fireBall.png')
+    this.load.image('fireDrag', 'assets/fireDrag.png')
   },
 
   create: function () {
@@ -39,7 +49,7 @@ let gameState = {
     mob.enableBody = true
     mob.physicsBodyType = Phaser.Physics.ARCADE
 
-    badyCreation()
+    baddieCreation()
     mob.setAll('anchor.x', 0.5)
     mob.setAll('anchor.y', 0.5)
 
@@ -48,47 +58,65 @@ let gameState = {
     bullets.enableBody = true
     bullets.physicsBodyType = Phaser.Physics.ARCADE
 
-    bullets.createMultiple(40, 'fireBall')
+    bullets.createMultiple(40, 'fireDrag')
     bullets.setAll('anchor.x', 0.5)
     bullets.setAll('anchor.y', 0.5)
+
+    //score text
+    killString = 'Deamons Slain : '
+    scoreText = game.add.text(10, 10, killString + kills, {font: '34px Arial', fill: "#fff"})
+    scoreText.fixedToCamera = true
+    //lives text
+    livesString = 'Lives : '
+    lifeText = game.add.text(10, 50, livesString + playerLife, {font: '34px Arial', fill: "#fff"})
+    lifeText.fixedToCamera = true
 
   },
 
   update: function () {
-    //player movement
+
+    //player rotates towards the pointer
     player.rotation = game.physics.arcade.angleToPointer(player)
 
     mob.forEach(function(el){
-      //bady movement
+      //baddie movement + physics + anchor point
       el.anchor.setTo(.5,.5)
       game.physics.arcade.enable(el)
       el.rotation = game.physics.arcade.angleBetween(el, player) + 1.57079633
       game.physics.arcade.moveToObject(el, player, 100)
 
       bullets.forEach(function (bu){
-        //this check to see if a bullet hit a bady
-        game.physics.arcade.overlap(bu, el, badyDeath)
+        //this check to see if a bullet hit a baddie
+        game.physics.arcade.overlap(bu, el, baddieDeath) //check if baddie gets hit
         if(invincible === false){
-          //this check to see if a bady hit the player
-          game.physics.arcade.overlap(player, el, wizDeath)
+          //this check to see if a baddie hit the player
+          game.physics.arcade.overlap(player, el, wizDeath) //check if baddie hits player
         }
         else {}
 
-        function badyDeath (){
-          el.kill()
-          bu.kill()
+        function baddieDeath (){
+          // after the baddie gets hit with a bullet
+          el.kill() //the baddie dies
+          bu.kill() //the bullet dies
+          kills++ //the score goes up
+          scoreText.text = killString + kills // update score board
         }
         function wizDeath (){
 
-          playerLife -= 1
           player.kill()
           invincible = true
+          playerLife-- //the live goes down
+          lifeText.text = livesString + playerLife // update life text
+          scoreText.text = killString + kills // update score board
 
           if(playerLife > 0){
+            //this resets the player
             player.reset(game.world.centerX, game.world.centerY)
+            //this keeps you in invincible mode for 2000 units of time
             game.time.events.add(2000, () => invincible = false)
           }
-          else{
+          else if(playerLife === 0){
+            lifeText.text = 'YOU ARE DEAD' // update life text
             // game over, click to restart
           }
         }
@@ -109,15 +137,14 @@ let gameState = {
       player.y += 4
     }
 
-    //bady spawner
-    if(total < 1000 && game.time.now > timer){
-      badyCreation()
+    //baddie spawner
+    if(total < 20 && game.time.now > timer){
+      baddieCreation()
     }
 
     // shooting fireBall
     if(game.input.activePointer.isDown){
       fireBullet()
-      var theIF = true
     }
 
   }
@@ -140,8 +167,8 @@ function fireBullet () {
     }
 }
 
-function badyCreation(){
-  mob.add(game.add.sprite(game.world.randomX, game.world.randomY, 'bady'))
+function baddieCreation(){
+  mob.add(game.add.sprite(game.world.randomX, game.world.randomY, 'baddie'))
 
   total++
   timer = game.time.now + 100
